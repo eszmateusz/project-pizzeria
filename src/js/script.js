a81426aa22043bd0ec9bedb06c086d8f0099e41e
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2020-10-30 18:11:55 +0100

    Add showing and hiding cart

--- src/js/script.js
+++ src/js/script.js
@@ -323,6 +323,42 @@
     }
   }
 
+  class Cart{
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      console.log('thisCart.dom.wrapper:', thisCart.dom.wrapper);
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
+        console.log('clicked');
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+        console.log('thisCart.dom.wrapper:', thisCart.dom.wrapper);
+      });
+    }
+  }
+
   const app = {
     initMenu: function() {
       const thisApp = this;
@@ -339,6 +375,13 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function() {
       const thisApp = this;
       //console.log('*** App starting ***');
@@ -349,6 +392,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
